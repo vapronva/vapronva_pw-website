43925e6b583d7fd89b9b6f6550a35495fe5aed1e
Vladimir (vapronva) <vapronhm@pm.me>
2022-03-30 14:00:43 +0300

    Update publish-dc.sh

--- publish-dc.sh
+++ publish-dc.sh
@@ -2,5 +2,6 @@
 set -e
 
 docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD" registry.vapronva.pw
+docker pull registry.vapronva.pw/websites/vapronva_pw-website:latest
 docker-compose -f 'docker-compose.yml' --project-name 'w-vapronvapw' down
 docker-compose -f 'docker-compose.yml' --project-name 'w-vapronvapw' up -d
