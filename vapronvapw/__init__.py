f24e9b8c8bf23f52e92ad95d12617ba6c7a36144
Vladimir (vapronva) <vapronhm@pm.me>
2022-05-28 23:04:17 +0300

    Add static files, switch to local storage, fixes in tests

--- vapronvapw/__init__.py
+++ vapronvapw/__init__.py
@@ -1,4 +1,5 @@
-from vapronvapw import views  # skipcq: PY-W2000
 from flask import Flask
 
 app = Flask("web-vapronvapw", template_folder="vapronvapw/templates")
+
+from vapronvapw import views  # skipcq: PY-W2000
